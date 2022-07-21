688d57f54c38aec22cae81d287f4e28df1144285
Lucas de Figueiredo Aguiar <lukasfaguiar@gmail.com>
2022-07-21 16:12:15 -0300

    DSA

---
+++ DSA/Atividades Cap 02/Metodos e Funcoes/atv03.py
@@ -0,0 +1,16 @@
+# Exercício 3 - Crie uma função que receba como parâmetro uma lista de 4 elementos, adicione 2 elementos a lista e
+# imprima a lista
+lista = ['lucas', 'mateus', 'amarildo', 'santana']
+
+print(lista)
+
+def adicionaelementos(listarecebida):
+    listarecebida.append('elias')
+    listarecebida.append('gustavo')
+    return
+
+
+adicionaelementos(lista)
+
+print(lista)
+
---
+++ DSA/Atividades Cap 02/Metodos e Funcoes/atv08.py
@@ -0,0 +1,5 @@
+# Exercício 8
+# Crie um dicionário e liste todos os métodos e atributos do dicionário
+dicionario = {'lucas': 25, 'mateus': 35}
+
+print(dir(dicionario))
